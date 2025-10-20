9786be5f89017d359d92874f513e99a8371c06fd
Richard Gao <richard@sgnl.ai>
2025-10-20 22:02:48 +0000

    Add more fields to logger config

--- pkg/logger/config.go
+++ pkg/logger/config.go
@@ -1,10 +1,24 @@
 package logger
 
-import "github.com/spf13/viper"
+import (
+	"github.com/spf13/viper"
+)
 
 type Config struct {
+	// Mode sets the logging mode. Valid modes are: "console", "file".
+	Mode []string `yaml:"mode" json:"mode" mapstructure:"mode"`
 	// Level sets the logging level. Valid levels are: "DEBUG", "INFO", "WARN", "ERROR", "DPANIC", "PANIC", and "FATAL".
 	Level string `json:"level"`
+
+	// The following fields are only used if "file" is included in Mode.
+	// FilePath sets the file path for file logging.
+	FilePath string `yaml:"file_path" json:"file_path" mapstructure:"file_path"`
+	// FileMaxSize sets the maximum size in megabytes of the log file before it gets rotated.
+	FileMaxSize int `yaml:"file_max_size" json:"file_max_size" mapstructure:"file_max_size"`
+	// FileMaxBackups sets the maximum number of old log files to retain.
+	FileMaxBackups int `yaml:"file_max_backups" json:"file_max_backups" mapstructure:"file_max_backups"`
+	// FileMaxDays sets the maximum number of days to retain old log files.
+	FileMaxDays int `yaml:"file_max_days" json:"file_max_days" mapstructure:"file_max_days"`
 }
 
 func LoadConfig() Config {
@@ -13,10 +27,15 @@ func LoadConfig() Config {
 	v.AutomaticEnv()
 
 	v.SetDefault("level", "INFO")
+	v.SetDefault("mode", "console")
+	v.SetDefault("file_path", "/var/log/sgnl/unconfigured.log")
+	v.SetDefault("file_max_size", 100)
+	v.SetDefault("file_max_days", 7)
+	v.SetDefault("file_max_backups", 10)
 
-	cfg := Config{
-		Level: v.GetString("level"),
-	}
+	var cfg Config
+
+	v.UnmarshalExact(&cfg)
 
 	return cfg
 }
